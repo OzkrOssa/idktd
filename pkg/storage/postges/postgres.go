6df10feaa7d1f4b1cdea7b9f06eab05e41641db9
Oscar Ossa <oskrlex17@gmail.com>
2024-12-20 15:27:50 -0500

    feat(postgres): add PostgreSQL database connection wrapper with query builder

---
+++ pkg/storage/postges/postgres.go
@@ -0,0 +1,67 @@
+package postgres
+
+import (
+	"context"
+	"errors"
+	"fmt"
+
+	"github.com/Masterminds/squirrel"
+	"github.com/OzkrOssa/idktd/pkg/config"
+	"github.com/jackc/pgx/v5/pgconn"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+/**
+ * DB is a wrapper for PostgreSQL database connection
+ * that uses pgxpool as database driver.
+ * It also holds a reference to squirrel.StatementBuilderType
+ * which is used to build SQL queries that compatible with PostgreSQL syntax
+ */
+type DB struct {
+	*pgxpool.Pool
+	*squirrel.StatementBuilderType
+	url string
+}
+
+// New creates a new PostgreSQL database instance
+func New(ctx context.Context, config *config.DB) (*DB, error) {
+	url := fmt.Sprintf("%s://%s:%s@%s:%s/%s?sslmode=disable",
+		config.Connection,
+		config.User,
+		config.Password,
+		config.Host,
+		config.Port,
+		config.Name,
+	)
+	pool, err := pgxpool.New(ctx, url)
+	if err != nil {
+		return &DB{}, err
+	}
+
+	err = pool.Ping(ctx)
+	if err != nil {
+		return &DB{}, err
+	}
+
+	psql := squirrel.StatementBuilder.PlaceholderFormat(squirrel.Dollar)
+
+	db := &DB{
+		pool,
+		&psql,
+		url,
+	}
+
+	return db, nil
+}
+
+// ErrorCode returns the error code of the given error
+func (db *DB) ErrorCode(err error) string {
+	var pgErr *pgconn.PgError
+	errors.As(err, &pgErr)
+	return pgErr.Code
+}
+
+// Close closes the database connection
+func (db *DB) Close() {
+	db.Pool.Close()
+}
